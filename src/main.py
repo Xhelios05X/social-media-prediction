8a677a1c1d14b5e421b7247885d8109c0684b3cb
BartłomiejGazduslki <heluios05@gmail.com>
2026-05-08 09:42:02 +0200

    added dataset split, some comments

--- src/main.py
+++ src/main.py
@@ -1,17 +1,36 @@
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 FILENAME = "../Teen_Mental_Health_Dataset.csv"
 
 def data_preprocessing(filename = FILENAME) -> object:
     data = np.genfromtxt(fname=FILENAME, delimiter=",", dtype=None, skip_header=1, encoding='utf-8')
     
+    """
+    data binarization
+    female == 0
+    male == 1
+    """
     gender_encoded = np.where(data['f1'] == 'female', 1, 0)
 
+    """
+    social ineraction encoding
+    low == 0
+    medium == 1
+    high == 2 
+    """
     interaction_encoded = np.zeros(data.shape[0])
     interaction_encoded[data['f8'] == 'medium'] = 1
     interaction_encoded[data['f8'] == 'high'] = 2
 
+
+    """
+    create new array to data encoding
+    is_instagram == 1/0 (True/False)
+    is_tiktok == 1/0 (True/False)
+    is_both = = 1/0 (True/False)
+    """
     is_instagram = np.where(data['f3'] == 'Instagram', 1, 0)
     is_tiktok = np.where(data['f3'] == 'TikTok', 1, 0)
     is_both = np.where(data['f3'] == 'Both', 1, 0)
@@ -41,8 +60,13 @@ def data_preprocessing(filename = FILENAME) -> object:
 def main() -> None:
     X, y = data_preprocessing()
 
+    test_size = 0.5
+    random_state = 2
+
+    x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+
     scaler = MinMaxScaler()
-    X_normalized = scaler.fit_transform()
+    X_normalized = scaler.fit_transform(x_train)
     
 
 
